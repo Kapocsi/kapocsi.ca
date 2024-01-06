093b63475f77c3f636b78037accd4233eae1942b
Thomas Kapocsi <tkapo261@mtroyal.ca>
2024-01-06 10:37:43 -0700

    fixes

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,7 @@
 import mysql from "mysql2/promise";
 
+//mysql://ksfb4u87utz58vxkkh6w:pscale_pw_QTSJ0wSus9pcjs3eMKEzi4YQvsYGrQeBUZ6nvRS5GSa@aws.connect.psdb.cloud/homepage?ssl={"rejectUnauthorized":true}
+
 const pool = mysql.createPool({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USERNAME,
@@ -8,7 +10,7 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  ssl: { ca: "/etc/ssl/certs/ca-certificates.crt" },
+  ssl: process.env.DATABASE_HOST !== "localhost" ? { rejectUnauthorized: true } : undefined,
 });
 
 export default pool;
