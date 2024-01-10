554b2a773033dcc53de72ad04b586ab830a9fd0f
Thomas Kapocsi <tkapo261@mtroyal.ca>
2024-01-09 18:03:05 -0700

    changes

--- src/app/articles.tsx
+++ src/app/articles.tsx
@@ -1,20 +1,20 @@
-export const dynamic = "force dynamic";
 export const revalidate = 1;
 
-import pool from "@/db";
-import { FieldPacket, RowDataPacket } from "mysql2";
 import Link from "next/link";
-
-async function get_articles() {
-  "force dynamic";
-  let [articles]: [RowDataPacket[], FieldPacket[]] = await pool.query(
-    "SELECT path, title, date_added FROM blogs where mime like'%text/html%' ORDER BY date_modified DESC; ",
-  );
-  return articles;
-}
+import { get_articles } from "./get_articles";
 
 export default async function Article() {
-  const articles = await get_articles();
+  let articles = await get_articles();
+
+  const rows = articles.map((data, key) => (
+    <tr key={key} className="border-[var(--f\_high)] border-b [&>td]:pt-2">
+      <td>
+        <Link href={data.path}>{data.title}</Link>
+      </td>
+      <td className="text-right">{data.date_added.toLocaleDateString()}</td>
+    </tr>
+  ));
+
   return (
     <table className="table-fixed w-[100%]">
       <thead className="border-[var(--f\_high)] border-b-2">
@@ -23,16 +23,7 @@ export default async function Article() {
           <td className="text-right"> Date Published </td>
         </tr>
       </thead>
-      <tbody>
-        {articles.map((data, key) => (
-          <tr key={key} className="border-[var(--f\_high)] border-b [&>td]:pt-2">
-            <td>
-              <Link href={data.path}>{data.title}</Link>
-            </td>
-            <td className="text-right">{data.date_added.toLocaleDateString()}</td>
-          </tr>
-        ))}
-      </tbody>
+      <tbody>{rows}</tbody>
     </table>
   );
 }
