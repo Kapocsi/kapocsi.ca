3bc76fc49a47f4528e89fbf4c699dbd4948ef458
Thomas Kapocsi <tkapo261@mtroyal.ca>
2024-01-09 18:08:22 -0700

    changes

--- src/app/get_articles.tsx
+++ src/app/get_articles.tsx
@@ -2,6 +2,7 @@ import pool from "@/db";
 import { FieldPacket, RowDataPacket } from "mysql2";
 
 export async function get_articles() {
+  console.log("HIT");
   let [articles]: [RowDataPacket[], FieldPacket[]] = await pool.query(
     "SELECT path, title, date_added FROM blogs where mime like'%text/html%' ORDER BY date_modified DESC; ",
   );
