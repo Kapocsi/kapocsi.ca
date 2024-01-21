e78099ef672d83b56182cd48aa91e3b02073cd65
Thomas Kapocsi <tomkap011@gmail.com>
2024-01-21 13:46:39 -0700

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Old_Standard_TT } from "next/font/google";
 import "./globals.css";
 import HundredRabbitsThemeDrop from "@/lib/100rTheme";
+import { Analytics } from "@vercel/analytics/react";
 
 const old_standard_tt = Old_Standard_TT({
   subsets: ["latin-ext"],
@@ -19,6 +20,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <html lang="en">
         <body className={old_standard_tt.className + " bg-[--background]"} suppressHydrationWarning={true}>
           <HundredRabbitsThemeDrop>{children}</HundredRabbitsThemeDrop>
+          <Analytics />
         </body>
       </html>
     </>
