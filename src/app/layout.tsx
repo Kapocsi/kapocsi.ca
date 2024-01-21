7a0acf676cfdafd0fefebed640d2ba83765ed077
Thomas Kapocsi <tomkap011@gmail.com>
2024-01-21 16:19:45 -0700

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,20 +18,20 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <>
       <html lang="en">
-        <body
-          className={old_standard_tt.className + " bg-[--background] min-h-screen flex flex-col "}
-          suppressHydrationWarning={true}
-        >
-          <TopBar></TopBar>
-          <div className="flex-grow">
-            <HundredRabbitsThemeDrop>
+        <HundredRabbitsThemeDrop>
+          <body
+            className={old_standard_tt.className + " bg-[--background] min-h-screen flex flex-col "}
+            suppressHydrationWarning={true}
+          >
+            <TopBar></TopBar>
+            <div className="flex-grow">
               <div className="w-1/2 m:w-3/4 mx-auto">{children}</div>
-            </HundredRabbitsThemeDrop>
-          </div>
-          <div className="align-bottom py-5 sm:columns-3 columns-1">
-            <ContactSection />
-          </div>
-        </body>
+            </div>
+            <div className="align-bottom py-5 sm:columns-3 columns-1">
+              <ContactSection />
+            </div>
+          </body>
+        </HundredRabbitsThemeDrop>
       </html>
     </>
   );
