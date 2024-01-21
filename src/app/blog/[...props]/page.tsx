8fd35eac955d1faa4e0cd718bb9b7f8ad6881e4e
Thomas Kapocsi <tomkap011@gmail.com>
2024-01-21 15:27:37 -0700

    changes

--- src/app/blog/[...props]/page.tsx
+++ src/app/blog/[...props]/page.tsx
@@ -6,7 +6,7 @@ import MathJax from "./mathjax";
 
 export default async function Blogs({ params }: { params: { props: string[] } }) {
   const path = decodeURI(`/blog/${params.props.join("/")}`);
-  const query = "SELECT data, mime FROM blogs WHERE path = ?";
+  const query = "SELECT data, mime, title, date_added, date_modified FROM blogs WHERE path = ?";
   const [results] = (await pool.query(query, [path])) as RowDataPacket[];
 
   const blogData = results[0];
@@ -19,11 +19,13 @@ export default async function Blogs({ params }: { params: { props: string[] } })
   }
 
   return (
-    <div className="flex justify-center" id="blog-article">
+    <div className="flex-col" id="blog-article">
       <MathJax />
-      <div className=" lg:w-1/2">
-        <article dangerouslySetInnerHTML={{ __html: blogData.data.toString() }} />
+      <div className="border-b">
+        <h1 className="text-3xl">{blogData.title}</h1>
+        <p> Published {blogData.date_added.toLocaleDateString()}</p>
       </div>
+      <article dangerouslySetInnerHTML={{ __html: blogData.data.toString() }}></article>
     </div>
   );
 }
