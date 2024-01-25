136efbb459294667c02f31185b0bc9827144fe74
Thomas Kapocsi <tkapo261@mtroyal.ca>
2024-01-25 15:39:50 -0700

    fix

--- src/lib/100rTheme.tsx
+++ src/lib/100rTheme.tsx
@@ -61,8 +61,10 @@ function get_xml_content(file: File): Promise<Document> {
 const HundredRabitsDrop = ({ children }: Props) => {
   // check for local storage existing and apply theme
   useEffect(() => {
-    const colors = JSON.parse(localStorage.getItem("100rColors") as string);
-    set_colors(colors);
+    const colors = JSON.parse(localStorage.getItem("100rColors"));
+    if (colors) {
+      set_colors(colors);
+    }
   }, []);
 
   const onDrop = (acceptedFiles: File[]) => {
