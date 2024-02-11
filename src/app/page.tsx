bf8a5cacc60cbaf4d140a9a75aca5f2f2b7775ff
Thomas Kapocsi <tomkap011@gmail.com>
2024-02-10 17:44:29 -0700

    change revalidate time for testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 export const dynamic = "force-dynamic";
-export const revalidate = 3600;
+export const revalidate = 10;
 
 import { ContactSection } from "@/lib/ContactSection";
 import Articles from "./articles";
