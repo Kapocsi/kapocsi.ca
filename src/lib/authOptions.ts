f3b1d18c1780d3348f412cf231c06f8e9dedacb6
Thomas Kapocsi <tkapo261@mtroyal.ca>
2024-02-12 20:12:28 -0700

    I fucking hate TS

--- src/lib/authOptions.ts
+++ src/lib/authOptions.ts
@@ -8,12 +8,12 @@ export const authOptions = {
   adapter: DrizzleAdapter(db) as Adapter,
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
     }),
     GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
     }),
   ],
 };
