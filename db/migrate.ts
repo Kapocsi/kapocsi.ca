15b3f4148bf22df8d0cdb66a0f032f0a16644663
Thomas Kapocsi <tkapo261@mtroyal.ca>
2024-02-12 21:44:04 -0700

    changes

---
+++ db/migrate.ts
@@ -0,0 +1,15 @@
+import { drizzle } from "drizzle-orm/mysql2";
+import { migrate } from "drizzle-orm/mysql2/migrator";
+import { createConnection } from "mysql2/promise";
+import { auth } from "@/db";
+
+async function main() {
+  console.log(auth);
+
+  const connection = await createConnection(auth);
+  const db = drizzle(connection);
+  await migrate(db, { migrationsFolder: "drizzle" });
+  await connection.end();
+}
+
+main();
--- src/db.ts
+++ src/db.ts
@@ -1,16 +1,24 @@
 export const runtime = "edge";
 
+import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
+import * as schema from "../db/schema";
+import dotenv from "dotenv";
+dotenv.config();
 
-const pool = mysql.createPool({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+export const auth = {
+  host: process.env.DATABASE_HOST as string,
+  user: process.env.DATABASE_USERNAME as string,
+  password: process.env.DATABASE_PASSWORD as string,
+  database: process.env.DATABASE_NAME as string,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  ssl: process.env.DATABASE_HOST !== "localhost" ? { rejectUnauthorized: true } : undefined,
-});
+  // ssl: process.env.DATABASE_HOST !== "localhost" ? { rejectUnauthorized: true } : undefined,
+};
+
+const pool = mysql.createPool(auth);
 
 export default pool;
+
+export const db = drizzle(pool, { schema, mode: "planetscale" });
