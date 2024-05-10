68c929fc6c0869cdead9514c1a6ec2ac6f67fc79
Thomas Kapocsi <tkapo261@mtroyal.ca>
2024-05-10 10:37:55 -0600

    update deps

--- src/lib/authOptions.ts
+++ src/lib/authOptions.ts
@@ -23,7 +23,7 @@ export const authOptions = {
     EmailProvider({
       server: {
         host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
+        port: parseInt(process.env.EMAIL_PORT ?? ""),
         auth: {
           user: process.env.EMAIL_USER,
           pass: process.env.EMAIL_PASSWORD,
